ae692e88d1d156890241615c3d8e0acbbdb7bb7f
sophiajm <sofiajmarinova@gmail.com>
2020-09-03 14:02:18 -0400

    add dynamic GET route

--- server.js
+++ server.js
@@ -61,7 +61,10 @@ app.post('/login',function(req,res){
     }
 })
 
-
+// Before closing out this course, try a few more things in Postman:
+ // Change the password value in the body tab to see what an incorrect password response looks like
+ // Try our earlier requests that we made from the browser (remember to switch to GET in Postman)
+ // See if you can make a new endpoint in the server.js code, then call it from Postman
 
 app.listen(8000, function(){
   console.log("server is listening")
